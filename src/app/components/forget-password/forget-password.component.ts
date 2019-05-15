4986fdfeb401201e028618e011d1524c16d4c394
Saeed Ansari <saeedansari@Saeeds-MacBook-Pro.local>
2019-05-15 18:49:09 -0400

    incremental checkin
    Signed-off-by: Saeed Ansari <saeedansari@Saeeds-MacBook-Pro.local>

--- src/app/components/forget-password/forget-password.component.ts
+++ src/app/components/forget-password/forget-password.component.ts
@@ -1,14 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { UserService } from '../../providers/user-service';
+import { ModalController, NavParams, NavController, ToastController } from '@ionic/angular';
 
+
+
+/**
+ * Generated class for the ForgetPasswordComponent component.
+ *
+ * See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
+ * for more info on Angular Components.
+ */
 @Component({
-  selector: 'app-forget-password',
-  templateUrl: './forget-password.component.html',
-  styleUrls: ['./forget-password.component.scss'],
+  selector: 'forget-password',
+  templateUrl: 'forget-password.html',
+  providers: [UserService]
+
 })
-export class ForgetPasswordComponent implements OnInit {
+export class ForgetPasswordComponent {
 
-  constructor() { }
+  public email = "";
 
-  ngOnInit() {}
+  text: string;
 
+  constructor(
+    private _user: UserService,
+    public vc: ModalController,
+    private toastCtrl: ToastController
+  ) {
+
+  }
+
+  async presentToast(message: string) {
+    let toast = await this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss().then(() => {
+      console.log('Dismissed toast');
+    });
+
+    await toast.present();
+  }
+
+  sendForgetPassEmail() {
+    if (this.email != "") {
+      this._user.EmailUserForgetPassword(this.email).subscribe(sub => {
+
+        this.presentToast("Email sent to " + this.email);
+
+        this.vc.dismiss();
+        /*
+        setTimeout(function () {
+          this.vc.dismiss();
+        }, 4000);
+        */
+
+      })
+    }
+    else {
+      this.presentToast("Please enter an Email you registered with Communities");
+    }
+    console.log(this.email);
+  }
+
+  closeModal() {
+
+    this.vc.dismiss();
+  }
 }
